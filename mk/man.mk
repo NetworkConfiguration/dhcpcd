8d6ce033621674d66c098126f0429100f68af21b
Roy Marples <roy@marples.name>
2008-01-08 10:22:57 +0000

    Fix the man install

--- mk/man.mk
+++ mk/man.mk
@@ -1,11 +1,14 @@
 # rules to install manpages
 # Copyright 2008 Roy Marples <roy@marples.name>
 
-MANPREFIX?=	/usr/share
+SHAREDIR?=	/usr/share
+MANDIR?=	${SHAREDIR}/man/man
 MANMODE?=	0444
 MINSTALL?=	${INSTALL} -m ${MANMODE}
 
+man: ${MAN}
+
 # We cheat as all our pages go into section 8
-maninstall: ${MAN}
-	${INSTALL} -d ${DESTDIR}${MANPREFIX}/man/man8
-	for man in ${MAN}; do ${MINSTALL} $$man ${DESTDIR}${MANPREFIX}/man/man8; done
+maninstall: man
+	${INSTALL} -d ${DESTDIR}${MANDIR}8
+	for man in ${MAN}; do ${MINSTALL} $$man ${DESTDIR}${MANDIR}8; done
