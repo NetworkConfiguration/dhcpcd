ce51e90ffa9b9fc2001f0918ffee28201e5bfd70
Roy Marples <roy@marples.name>
2008-03-27 20:10:31 +0000

    If not Linux then assume BSD.

--- mk/os.mk
+++ mk/os.mk
@@ -1,7 +1,7 @@
 # Setup OS specific variables
 # Copyright 2008 Roy Marples <roy@marples.name>
 
-_UNAME_S_SH=	case `uname -s` in *BSD|DragonFly) echo "BSD";; *) uname -s;; esac
-_UNAME_S!=	${_UNAME_S_SH}
-UNAME_S=	${_UNAME_S}$(shell ${_UNAME_S_SH})
-include ${MK}/os-${UNAME_S}.mk
+_OS_SH=	case `uname -s` in Linux) echo "Linux";; *) echo "BSD";; esac
+_OS!=		${_OS_SH}
+OS=		${_OS}$(shell ${_OS_SH})
+include ${MK}/os-${OS}.mk
