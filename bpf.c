f3053f4759310fd6f2fefa59786685c0c9253bd4
Roy Marples <roy@marples.name>
2008-05-15 14:20:01 +0000

    No need for this check with our non blocking sockets.

--- bpf.c
+++ bpf.c
@@ -159,11 +159,6 @@ get_packet(struct interface *iface, void *data, ssize_t len)
 	ssize_t bytes;
 	const unsigned char *payload, *d;
 
-	if (iface->buffer_pos > iface->buffer_len) {
-		iface->buffer_len = iface->buffer_pos = 0;
-		return 0;
-	}
-
 	for (;;) {
 		if (iface->buffer_len == 0) {
 			bytes = read(iface->fd, iface->buffer,
