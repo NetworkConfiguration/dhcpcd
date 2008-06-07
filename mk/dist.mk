29fd7eaf8c059d35815ea74ed63f318619f15744
Roy Marples <roy@marples.name>
2008-06-07 00:07:05 +0000

    Allow building of snapshots.

--- mk/dist.mk
+++ mk/dist.mk
@@ -5,11 +5,27 @@ GITREF?=	HEAD
 DISTPREFIX?=	${PROG}-${VERSION}
 DISTFILE?=	${DISTPREFIX}.tar.bz2
 
-CLEANFILES+=	${DISTFILE}
+CLEANFILES+=	*.tar.bz2
 
 _VERSION_SH=	sed -n 's/\#define VERSION[[:space:]]*"\(.*\)".*/\1/p' config.h
 _VERSION!=	${_VERSION_SH}
 VERSION=	${_VERSION}$(shell ${_VERSION_SH})
 
+_SNAP_SH=	date -u +%Y%m%d%H%M
+_SNAP!=		${_SNAP_SH}
+SNAP=		${_SNAP}$(shell ${_SNAP_SH})
+SNAPDIR=	${DISTPREFIX}-${SNAP}
+SNAPFILE=	${SNAPDIR}.tar.bz2
+
 dist:
 	git archive --prefix=${DISTPREFIX}/ ${GITREF} | bzip2 > ${DISTFILE}
+
+snapshot:
+	mkdir /tmp/${SNAPDIR}
+	cp -RPp * /tmp/${SNAPDIR}
+	(cd /tmp/${SNAPDIR}; make clean)
+	tar -cvjpf ${SNAPFILE} -C /tmp ${SNAPDIR}
+	rm -rf /tmp/${SNAPDIR}
+	ls -l ${SNAPFILE}
+
+snap: snapshot
