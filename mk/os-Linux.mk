bae25b133bc5421dce1a92fcd404fa6e8ce68101
Roy Marples <roy@marples.name>
2008-07-20 19:33:27 +0000

    Remove the fork() test as compile hosts will normally have it, making it redundant.

--- mk/os-Linux.mk
+++ mk/os-Linux.mk
@@ -6,23 +6,3 @@ SRC_IF=		if-linux.c
 
 CPPFLAGS+=	-D_BSD_SOURCE -D_XOPEN_SOURCE=600
 LIBRT=		-lrt
-
-# Work out if our fork() works or not.
-# If cross-compiling, you'll need to set HAVE_FORK to yes or no depending
-# on your target arch.
-_HAVE_FORK_SH= if test "${HAVE_FORK}" = "yes"; then \
-		echo ""; \
-	elif test -n "${HAVE_FORK}"; then \
-		echo "-DTHERE_IS_NO_FORK"; \
-	else \
-		printf '\#include <stdlib.h>\n\#include <unistd.h>\nint main (void) { pid_t pid = fork(); if (pid == -1) exit (EXIT_FAILURE); exit (EXIT_SUCCESS); }\n' > .fork.c; \
-		${CC} .fork.c -o .fork >/dev/null 2>&1; \
-		if ./.fork; then \
-			echo ""; \
-		else \
-			echo "-DTHERE_IS_NO_FORK"; \
-		fi; \
-		rm -f .fork.c .fork; \
-	fi;
-_HAVE_FORK!=	${_HAVE_FORK_SH}
-CPPFLAGS+=	${_HAVE_FORK}$(shell ${_HAVE_FORK_SH})
