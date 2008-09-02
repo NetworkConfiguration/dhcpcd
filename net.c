948eea38e57ee86215613e14e48a4615196f2f6a
Roy Marples <roy@marples.name>
2008-09-02 15:35:02 +0000

    Don't need this.

--- net.c
+++ net.c
@@ -266,7 +266,6 @@ init_interface(const char *ifname)
 	memset(&ireq, 0, sizeof(ireq));
 	strlcpy(ireq.i_name, ifname, sizeof(ireq.i_name));
 	ireq.i_type = IEEE80211_IOC_NUMSSIDS;
-	ireq.i_val = 3;
 	if (ioctl(s, SIOCG80211, &ireq) != -1)
 		iface->metric += 100; 
 #endif
