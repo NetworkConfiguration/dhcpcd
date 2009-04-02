919a8c8bd4f2a3106ae0db58c7e323c89f50ba74
Roy Marples <roy@marples.name>
2009-04-02 09:32:54 +0000

    Release dhcpcd-4.99.16

--- config.h
+++ config.h
@@ -28,7 +28,7 @@
 #define CONFIG_H
 
 #define PACKAGE			"dhcpcd"
-#define VERSION			"4.99.15"
+#define VERSION			"4.99.16"
 
 /* Some systems do not have a working fork. */
 /* #define THERE_IS_NO_FORK */
