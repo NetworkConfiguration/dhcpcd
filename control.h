2e21d85f7a63e938b34c11cde49c380ee8ccd183
Roy Marples <roy@marples.name>
2014-09-15 22:50:44 +0000

    Fix FD_LISTEN | FD_UNPRIV flags

--- control.h
+++ control.h
@@ -51,7 +51,7 @@ struct fd_list {
 };
 TAILQ_HEAD(fd_list_head, fd_list);
 
-#define FD_LISTEN	(0<<1)
+#define FD_LISTEN	(1<<0)
 #define FD_UNPRIV	(1<<1)
 
 int control_start(struct dhcpcd_ctx *, const char *);
