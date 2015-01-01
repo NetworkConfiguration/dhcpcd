adf84d84ddd73408719e20505f72f94e1ac1a30b
Roy Marples <roy@marples.name>
2015-01-01 20:50:48 +0000

    Welcome to 2015!

--- dhcpcd.c
+++ dhcpcd.c
@@ -1,6 +1,6 @@
 /*
  * dhcpcd - DHCP client daemon
- * Copyright (c) 2006-2014 Roy Marples <roy@marples.name>
+ * Copyright (c) 2006-2015 Roy Marples <roy@marples.name>
  * All rights reserved
 
  * Redistribution and use in source and binary forms, with or without
@@ -25,7 +25,7 @@
  * SUCH DAMAGE.
  */
 
-const char dhcpcd_copyright[] = "Copyright (c) 2006-2014 Roy Marples";
+const char dhcpcd_copyright[] = "Copyright (c) 2006-2015 Roy Marples";
 
 #define _WITH_DPRINTF /* Stop FreeBSD bitching */
 
