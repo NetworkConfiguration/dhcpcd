022ba947d284315fbd9dfc4807660f4efa6bbad9
Roy Marples <roy@marples.name>
2015-02-27 21:32:51 +0000

    Revert prior

--- ipv6.c
+++ ipv6.c
@@ -824,6 +824,14 @@ ipv6_addaddrs(struct ipv6_addrhead *addrs)
 }
 
 void
+ipv6_freeaddr(struct ipv6_addr *ap)
+{
+
+	eloop_q_timeout_delete(ap->iface->ctx->eloop, 0, NULL, ap);
+	free(ap);
+}
+
+void
 ipv6_freedrop_addrs(struct ipv6_addrhead *addrs, int drop,
     const struct interface *ifd)
 {
