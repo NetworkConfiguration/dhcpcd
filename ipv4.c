aceb692d4f47dc2c8f28554fcf5d17a103199c20
Roy Marples <roy@marples.name>
2015-09-08 16:29:02 +0000

    When deleting an old route and it doesn't have an interface then use the interface of the new route for context.

--- ipv4.c
+++ ipv4.c
@@ -440,6 +440,11 @@ nc_route(struct rt *ort, struct rt *nrt)
 			logger(nrt->iface->ctx, LOG_ERR, "if_route (CHG): %m");
 	}
 
+	/* If the old route does not have an interface, give it the
+	 * interface of the new route for context. */
+	if (ort && ort->iface == NULL)
+		ort->iface = nrt->iface;
+
 #ifdef HAVE_ROUTE_METRIC
 	/* With route metrics, we can safely add the new route before
 	 * deleting the old route. */
