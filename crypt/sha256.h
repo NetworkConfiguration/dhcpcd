526e316492a9bc784dc0f673cd0758380e719644
Roy Marples <roy@marples.name>
2016-01-14 18:10:35 +0000

    Remove unused prototypes.

--- crypt/sha256.h
+++ crypt/sha256.h
@@ -42,9 +42,5 @@ typedef struct SHA256Context {
 void	SHA256_Init(SHA256_CTX *);
 void	SHA256_Update(SHA256_CTX *, const void *, size_t);
 void	SHA256_Final(unsigned char [32], SHA256_CTX *);
-char   *SHA256_End(SHA256_CTX *, char *);
-char   *SHA256_File(const char *, char *);
-char   *SHA256_FileChunk(const char *, char *, off_t, off_t);
-char   *SHA256_Data(const void *, unsigned int, char *);
 
 #endif
