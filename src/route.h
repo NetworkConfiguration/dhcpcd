d2e5d3147b248007a74293057c8873e4e1664eae
Roy Marples <roy@marples.name>
2019-04-16 19:48:46 +0100

    Revert "route: Disable RT_FREE_ROUTE_TABLE by default."
    This reverts commit 46059a78ef33341de80c0085570f71bf0ec15e8b.

--- src/route.h
+++ src/route.h
@@ -40,8 +40,14 @@
 #include "dhcpcd.h"
 #include "sa.h"
 
-/* libc malloc should be good enough */
-#if defined(RT_FREE_ROUTE_TABLE) && RT_FREE_ROUTE_TABLE == 0
+/*
+ * Enable the route free list by default as
+ * memory usage is still reported as low/unchanged even
+ * when dealing with millions of routes.
+ */
+#if !defined(RT_FREE_ROUTE_TABLE)
+#define RT_FREE_ROUTE_TABLE
+#elif RT_FREE_ROUTE_TABLE == 0
 #undef RT_FREE_ROUTE_TABLE
 #endif
 
