7729116843a516006737b285f2a8004a1a666087
Roy Marples <roy@marples.name>
2019-04-24 12:18:25 +0100

    IPv6ND: Use generic initialisers rather than inet_pton

--- src/ipv6.h
+++ src/ipv6.h
@@ -44,9 +44,6 @@
 #  endif
 #endif
 
-#define	ALLNODES		"ff02::1"
-#define	ALLROUTERS		"ff02::2"
-
 #define EUI64_GBIT		0x01
 #define EUI64_UBIT		0x02
 #define EUI64_TO_IFID(in6)	do {(in6)->s6_addr[8] ^= EUI64_UBIT; } while (0)
