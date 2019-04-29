bdeeffdc770c53f7799d772d203e48e886b98238
Roy Marples <roy@marples.name>
2019-04-29 17:32:53 +0100

    dhcp: Ensure that we have DHCP running on the receiving interface
    ... before trying to process the received BOOTP message on it.

--- src/dhcp.c
+++ src/dhcp.c
@@ -3499,6 +3499,11 @@ dhcp_readudp(struct dhcpcd_ctx *ctx, struct interface *ifp)
 			logerr(__func__);
 			return;
 		}
+		if (D_CSTATE(ifp) == NULL) {
+			logdebugx("%s: received BOOTP for inactive interface",
+			    ifp->name);
+			return;
+		}
 	}
 
 	dhcp_handlebootp(ifp, (struct bootp *)buf, (size_t)bytes,
