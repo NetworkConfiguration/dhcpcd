a9882c1a32b800fda466c3c4af3819281bb98e00
Roy Marples <roy@marples.name>
2020-05-27 15:49:40 +0000

    Check AF_PACKET is defined

--- src/sa.c
+++ src/sa.c
@@ -32,7 +32,7 @@
 #include <arpa/inet.h>
 #ifdef AF_LINK
 #include <net/if_dl.h>
-#elif AF_PACKET
+#elif defined(AF_PACKET)
 #include <linux/if_packet.h>
 #endif
 
@@ -385,7 +385,7 @@ sa_addrtop(const struct sockaddr *sa, char *buf, socklen_t len)
 		}
 		return hwaddr_ntoa(CLLADDR(sdl), sdl->sdl_alen, buf, len);
 	}
-#elif AF_PACKET
+#elif defined(AF_PACKET)
 	if (sa->sa_family == AF_PACKET) {
 		const struct sockaddr_ll *sll;
 
