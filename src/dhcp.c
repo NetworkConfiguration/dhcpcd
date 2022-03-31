e92604fd76ae1d96b9503b7733255185ac9f6aa4
Seamus <seamusdemora@gmail.com>
2022-03-31 16:22:07 -0500

    Update dhcp.c

--- src/dhcp.c
+++ src/dhcp.c
@@ -4058,7 +4058,7 @@ dhcp_handleifa(int cmd, struct ipv4_addr *ia, pid_t pid)
 
 	ifo = ifp->options;
 	if (ifo->options & DHCPCD_INFORM) {
-		if (state->state != DHS_INFORM)
+		if (state->state != DHS_INFORM && state->state != DHS_BOUND)
 			dhcp_inform(ifp);
 		return ia;
 	}
