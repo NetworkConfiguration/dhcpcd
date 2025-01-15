5905fbd66d440fdc087e4667904dc2ae0a345feb
Roy Marples <roy@marples.name>
2025-01-15 18:51:46 +0000

    BSD: Note that to replicate prior we can't have a default route
    Add a comment for #425

--- src/if-bsd.c
+++ src/if-bsd.c
@@ -742,13 +742,13 @@ if_route(unsigned char cmd, const struct rt *rt)
 		{
 			rtm->rtm_index = (unsigned short)rt->rt_ifp->index;
 /*
- * OpenBSD rejects the message for on-link routes.
+ * OpenBSD rejects this for on-link routes when there is no default route
  * FreeBSD-12 kernel apparently panics.
  * I can't replicate the panic, but better safe than sorry!
- * https://roy.marples.name/archives/dhcpcd-discuss/0002286.html
  *
- * Neither OS currently allows IPv6 address sharing anyway, so let's
- * try to encourage someone to fix that by logging a waring during compile.
+ * Neither OS currently allows the same IPv6 address on different
+ * interfaces on the same network, so let's try to encourage someone to
+ * fix that by logging a waring during compile.
  */
 #if defined(__FreeBSD__) || defined(__OpenBSD__)
 #warning kernel does not allow IPv6 address sharing
