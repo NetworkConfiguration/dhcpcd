112cc12b8dabef6ca41558016e97bfa03b71df0d
Daniel Gröber <dxld@darkboxed.org>
2025-11-06 14:50:15 +0100

    manager: Fix loosing iface options on CARRIER
    When an interface (re-)gains carrier dhcpcd_handlecarrier() runs
    dhcpcd_initstate() to kick off profile re-selection. Previously this used
    args originally passed when starting the manager (ctx->argv).
    
    However interfaces started via the manager control
    interface (dhcpcd_initstate1() in dhcpcd_handleargs()) may be started with
    different args.
    
    For example if we start a manager with
    
        dhcpcd -M --inactive
    
    and then start only IPv4 on an interface with
    
        dhcpcd -4 iface0
    
    a subsequent CARRIER event will reset the interface to what amounts to
    "default config + `-M --inactive`" which in this case will enable ipv6
    also!
    
    To fix this we keep a copy of the arguments used to start an interface in
    the manager (dhcpcd_handleargs()) code path around around (ifp->argv).
    
    In the current implementation args passed for renew following the initial
    interface start will not be persisted. This causes the interface to reset
    to a state of "defaults + config + profile + start-cmdline".
    
    For example (continuing the scenario above) after enabling ipv6 with -n:
    
        $ dhcpcd -6 -n iface0
    
    A subsequent CARRIER event will disable ipv6 again as the effective
    arguments remain `-4 iface0` as passed during interface start.
    
    Note the per-interface daemon code path wasn't affected as ctx->args
    already contains the interface start args.

--- src/if-options.h
+++ src/if-options.h
@@ -322,4 +322,7 @@ int add_options(struct dhcpcd_ctx *, const char *,
 void free_dhcp_opt_embenc(struct dhcp_opt *);
 void free_options(struct dhcpcd_ctx *, struct if_options *);
 
+char **copy_argv(int argc, char **argv);
+void free_argv_copy(char **argv);
+
 #endif
